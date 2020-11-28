c6b67aa9231933218e026cc26ea97f79353bc737
mgdave2 <74630971+mgdave2@users.noreply.github.com>
2020-11-27 19:52:17 -0600

    new

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add any homepage scripting here!
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} // Add any homepage scripting here!
